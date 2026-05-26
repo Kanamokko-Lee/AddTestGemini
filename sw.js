3a9e9eb8e1e26ca3e0aae0b52b629695b32057ff
Kanamokko-Lee <happykanamo@gmail.com>
2026-05-27 07:23:42 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,6 @@
+// 全キャッシュ削除・無効化
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+  self.clients.claim();
+});
